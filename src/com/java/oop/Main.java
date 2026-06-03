7fe30669732153934d5c9426ced861bf09d7627e
Angasalavikas <angasalavikas123@gmail.com>
2026-06-03 22:11:04 +0530

    updated commit products.java

--- src/com/java/oop/Main.java
+++ src/com/java/oop/Main.java
@@ -1,16 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the
 package com.java.oop;
 public class Main{
     static void main() {
         products products;
-        products = new products();
-        products.id = 1;
-        products.name = "laptop";
-        products.maxRetailPrice = 39899;
-        products.discountPercentage = 20.5f;
-        products.rating = 4.5F;
-        products.isAvailable = true;
+        products = new products(1,"laptop",12000,3.5f,4.4f,true);
+//        products.id = 1;
+//        products.name = "laptop";
+//        products.maxRetailPrice = 39899;
+//        products.discountPercentage = 20.5f;
+//        products.rating = 4.5F;
+//        products.isAvailable = true;
+        System.out.println("id : " + products.id);
+        System.out.println("name : " + products.name);
+        System.out.println("MRP : " + products.maxRetailPrice);
+        System.out.println("discountPercentage : " + products.discountPercentage);
+        System.out.println("rating : " + products.rating);
+        System.out.println("isAvailable : " + products.isAvailable);
+        products products1 = new products(2,"car",8475,7.8f,3.4f,false);
         System.out.println("id : " + products.id);
         System.out.println("name : " + products.name);
         System.out.println("MRP : " + products.maxRetailPrice);
--- src/com/java/oop/products.java
+++ src/com/java/oop/products.java
@@ -7,4 +7,14 @@ public class products {
     float discountPercentage;
     float rating;
     boolean isAvailable;
+
+    public products(int id, String name, double maxRetailPrice, float discountPercentage, float rating, boolean isAvailable) {
+        this.id = id;
+        this.name = name;
+        this.maxRetailPrice = maxRetailPrice;
+        this.discountPercentage = discountPercentage;
+        this.rating = rating;
+        this.isAvailable = isAvailable;
+        System.out.println("products() is called :");
+    }
 }
