a78182fbcf5e88c75fecbb083fea5dd636db9bf9
Angasalavikas <angasalavikas123@gmail.com>
2026-06-03 12:08:51 +0530

    initial commit products.java

---
+++ src/com/java/oop/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package com.java.oop;
+public class Main{
+    static void main() {
+        products products;
+        products = new products();
+        products.id = 1;
+        products.name = "laptop";
+        products.maxRetailPrice = 39899;
+        products.discountPercentage = 20.5f;
+        products.rating = 4.5F;
+        products.isAvailable = true;
+        System.out.println("id : " + products.id);
+        System.out.println("name : " + products.name);
+        System.out.println("MRP : " + products.maxRetailPrice);
+        System.out.println("discountPercentage : " + products.discountPercentage);
+        System.out.println("rating : " + products.rating);
+        System.out.println("isAvailable : " + products.isAvailable);
+
+    }
+}
---
+++ src/com/java/oop/products.java
@@ -0,0 +1,10 @@
+package com.java.oop;
+
+public class products {
+    int id;
+    String name;
+    double maxRetailPrice;
+    float discountPercentage;
+    float rating;
+    boolean isAvailable;
+}
