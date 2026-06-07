ab8bf8ee42d1fc7d60d8f8cc8d39c90c9486a25d
Angasalavikas <angasalavikas123@gmail.com>
2026-06-07 17:39:14 +0530

    intial commit oop

---
+++ src/com/java/oop/inheritance/Main.java
@@ -0,0 +1,24 @@
+package com.java.oop.inheritance;
+
+public class Main {
+    static void main() {
+        Products product = new Products();
+       // System.out.println("id :" + product.id);
+        product.displayProductsDetails();
+        Products product2 = new Products(2,"product2",32424,4.5f,3.2f, true);
+        product2.displayProductsDetails();
+        Laptop laptop = new Laptop();
+        System.out.println(laptop.hardDisk);
+        System.out.println(laptop.ram);
+        System.out.println(laptop.operatingSystem);
+        Laptop laptop1 = new Laptop(23,"Mac Book",379373,38f,4.5f,true,130,4,"os");
+        laptop1.displayLaptopDetails();
+        Book book = new Book(14,"vikas",1000,20f,3.4f,false,"kittu",399,"balu");
+         book.displayBookDetails();
+         WindowsLaptop wIndowsLaptop = new WindowsLaptop();
+         Service service = new Service();
+         service.validate(wIndowsLaptop);
+         Service service1 = new Service();
+         service.validate(laptop);
+    }
+}
---
+++ src/com/java/oop/inheritance/Products.java
@@ -0,0 +1,32 @@
+package com.java.oop.inheritance;
+
+import com.java.oop.products;
+
+public class Products {
+    int id;
+    String name;
+    double maxRetailPrice;
+    float discountPercentage;
+    float rating;
+    boolean isAvailable;
+
+    public Products() {
+        System.out.println("constructor is called ");
+    }
+
+    public Products(int id, String name, double maxRetailPrice, float discountPercentage, float rating, boolean isAvailable) {
+        this.id = id;
+        this.name = name;
+        this.maxRetailPrice = maxRetailPrice;
+        this.discountPercentage = discountPercentage;
+        this.rating = rating;
+        this.isAvailable = isAvailable;
+    }
+    void displayProductsDetails(){
+        System.out.println("id :" + id);
+        System.out.println("name :" + name);
+        System.out.println("MRP :" + maxRetailPrice);
+        System.out.println("rating :" + rating);
+        System.out.println("isAvailable:" + isAvailable);
+    }
+}
