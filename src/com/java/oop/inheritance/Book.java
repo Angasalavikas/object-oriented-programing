891ee7ecd0cb141a1dfd294f2ae412131c6c9b43
Angasalavikas <angasalavikas123@gmail.com>
2026-06-07 17:39:30 +0530

    intial commit oop

---
+++ src/com/java/oop/inheritance/Book.java
@@ -0,0 +1,20 @@
+package com.java.oop.inheritance;
+
+public class Book extends Products{
+    String author;
+    int pages;
+    String publisher;
+
+    public Book(int id, String name, double maxRetailPrice, float discountPercentage, float rating, boolean isAvailable, String author, int pages, String publisher) {
+        super(id, name, maxRetailPrice, discountPercentage, rating, isAvailable);
+        this.author = author;
+        this.pages = pages;
+        this.publisher = publisher;
+    }
+    void displayBookDetails(){
+        displayProductsDetails();
+        System.out.println("author:" + author);
+        System.out.println("pages :" + pages);
+        System.out.println("publisher :" + publisher);
+    }
+}
