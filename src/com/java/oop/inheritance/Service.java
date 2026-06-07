1c89267b655f839913455ff0bb90d868cd1824d5
Angasalavikas <angasalavikas123@gmail.com>
2026-06-07 17:40:05 +0530

    intial commit oop

---
+++ src/com/java/oop/inheritance/Service.java
@@ -0,0 +1,13 @@
+package com.java.oop.inheritance;
+
+public class Service {
+    void validate(Products product){
+        System.out.println("validate product");
+    }
+    void validate(Laptop laptop){
+        System.out.println("validate laptop");
+    }
+    void validate(Book book){
+        System.out.println("validate book");
+    }
+}
