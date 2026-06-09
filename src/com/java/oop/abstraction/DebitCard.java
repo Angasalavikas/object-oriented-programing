97ca2a8defb1ca0292aa04464dcc5cbd03c228fb
Angasalavikas <angasalavikas123@gmail.com>
2026-06-09 19:20:54 +0530

    intial commit oop (abstract tightCouple)

--- src/com/java/oop/abstraction/DebitCard.java
+++ src/com/java/oop/abstraction/DebitCard.java
@@ -1,9 +1,9 @@
 package com.java.oop.abstraction;
 
-public class CreditCardPayment extends Payment {
+public class DebitCard extends Payment{
     @Override
     public boolean pay(double amount){
-        System.out.println("payment done using creditCard");
+        System.out.println("order process done using debit card");
         return true;
     }
 
