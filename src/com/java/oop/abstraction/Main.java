d8aebbb1577e923b2170553ec26dbdae002318af
Angasalavikas <angasalavikas123@gmail.com>
2026-06-11 23:42:31 +0530

    intial commit oop (abstract tightCouple)

--- src/com/java/oop/abstraction/Main.java
+++ src/com/java/oop/abstraction/Main.java
@@ -65,7 +65,6 @@ public class Main {
                 amount = sc.nextDouble();
                 System.out.println("Enter otp:");
                 otp = sc.nextInt();
-
                 payment.otpValidate(otp);
                 System.out.println("order process using upi");
                 break;
